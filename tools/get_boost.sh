cfb58faa668ce8cf3a77da092d589d477e96c156
Damian Jarek <damian.jarek93@gmail.com>
2019-05-22 23:11:41 +0200

    Update CI to Boost 1.70
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- tools/get_boost.sh
+++ tools/get_boost.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
-trap 'exit $?' ERR
-curl -L https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz -o boost.tar.gz
-tar -xf boost.tar.gz
-cd boost_*
-./bootstrap.sh
+set -e
 
-B2_VARIANT=$(echo $CMAKE_VARIANT | awk '{print tolower($0)}')
+# curl -L https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.gz -o boost.tar.gz
+# tar -xf boost.tar.gz
+# cd boost_*
+# ./bootstrap.sh
+#
+# B2_VARIANT=$(echo $CMAKE_VARIANT | awk '{print tolower($0)}')
+#
+# sudo ./b2 --with-system --with-atomic --with-thread --with-date_time --with-chrono --with-context cxxstd=11 variant=$B2_VARIANT toolset=$CC install
+# cd -
 
-sudo ./b2 --with-system --with-atomic --with-thread --with-date_time --with-chrono --with-context cxxstd=11 variant=$B2_VARIANT toolset=$CC install
-cd -
+SOURCE_DIR=$(pwd)
+cd ~
+git clone --depth 1 https://github.com/microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg install boost-asio
+cd $SOURCE_DIR
